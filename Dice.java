fe3754b25881bae4d1180ecc24e59488bb6852e8
Pat-rick-Hi-ck <patrick.hickey1@ucdconnect.ie>
2024-10-29 14:07:10 +0000

    Add files via upload

--- Dice.java
+++ Dice.java
@@ -1,2 +1,26 @@
+import java.util.*;
+
 public class Dice {
+    private static ArrayList<Integer> dice1;
+    private static ArrayList<Integer> dice2;
+    private static final Random random = new Random();
+
+    public Dice() {
+        dice1 = new ArrayList<>();
+        dice2 = new ArrayList<>();
+        initialiseDice();
+    }
+
+    private void initialiseDice() {
+        for (int i=1; i <= 6; i++) {
+            dice1.add(i);
+            dice2.add(i);
+        }
+    }
+
+    public static int[] roll() {
+        int dice1Value = dice1.get(random.nextInt(6));
+        int dice2Value = dice2.get(random.nextInt(6));
+        return new int[]{dice1Value, dice2Value};
+    }
 }
--- Game.java
+++ Game.java
@@ -1,9 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Game {
+    private Dice dice;
     private boolean running = true;
 
+    public Game() {
+        initialiseGame();
+    }
+
     private void initialiseGame() {
+        Board board = new Board();
+        dice = new Dice();
+
+        // add initial checkers
+        board.addChecker(23, new Checker(Checker.Colour.WHITE));
+        board.addChecker(23, new Checker(Checker.Colour.WHITE));
+        board.addChecker(18, new Checker(Checker.Colour.BLACK));
+        board.addChecker(18, new Checker(Checker.Colour.BLACK));
+        board.addChecker(18, new Checker(Checker.Colour.BLACK));
+        board.addChecker(18, new Checker(Checker.Colour.BLACK));
+        board.addChecker(18, new Checker(Checker.Colour.BLACK));
+        board.addChecker(16, new Checker(Checker.Colour.BLACK));
+        board.addChecker(16, new Checker(Checker.Colour.BLACK));
+        board.addChecker(16, new Checker(Checker.Colour.BLACK));
+        board.addChecker(12, new Checker(Checker.Colour.WHITE));
+        board.addChecker(12, new Checker(Checker.Colour.WHITE));
+        board.addChecker(12, new Checker(Checker.Colour.WHITE));
+        board.addChecker(12, new Checker(Checker.Colour.WHITE));
+        board.addChecker(12, new Checker(Checker.Colour.WHITE));
+        board.addChecker(11, new Checker(Checker.Colour.BLACK));
+        board.addChecker(11, new Checker(Checker.Colour.BLACK));
+        board.addChecker(11, new Checker(Checker.Colour.BLACK));
+        board.addChecker(11, new Checker(Checker.Colour.BLACK));
+        board.addChecker(11, new Checker(Checker.Colour.BLACK));
+        board.addChecker(7, new Checker(Checker.Colour.WHITE));
+        board.addChecker(7, new Checker(Checker.Colour.WHITE));
+        board.addChecker(7, new Checker(Checker.Colour.WHITE));
+        board.addChecker(5, new Checker(Checker.Colour.WHITE));
+        board.addChecker(5, new Checker(Checker.Colour.WHITE));
+        board.addChecker(5, new Checker(Checker.Colour.WHITE));
+        board.addChecker(5, new Checker(Checker.Colour.WHITE));
+        board.addChecker(5, new Checker(Checker.Colour.WHITE));
+        board.addChecker(0, new Checker(Checker.Colour.BLACK));
+        board.addChecker(0, new Checker(Checker.Colour.BLACK));
 
         System.out.println("Welcome to Backgammon");
         System.out.println();
@@ -11,13 +52,31 @@ public class Game {
 
     public void start() {
         Scanner scanner = new Scanner(System.in); // allows reading from the console
+        System.out.println("Enter name 1:");
+        String player1 = scanner.nextLine();
+        System.out.println( player1 + " plays with the White Checkers");
+        System.out.println("Enter name 2:");
+        String player2 = scanner.nextLine();
+        System.out.println( player2 + " plays with the Black Checkers");
+
         while (running) {
             Board.display();
-
             System.out.println("Enter move:");
-            String action = scanner.nextLine(); // stores user input into 'action'
-
+            String action = scanner.nextLine();// stores user input into 'action'
+            processAction(action);
         }
         scanner.close();
+    }
+
+    private void processAction(String action) {
+        action = action.toUpperCase();
+        if (action.equals("Q")) { // quit game
+            running = false;
+            System.out.println("Game over!");
+        } else if (action.equals("ROLL")) {
+            int[] roll = Dice.roll();
+            System.out.println("Dice roll:" + Arrays.toString(roll));
+        }
+
     }
 }
