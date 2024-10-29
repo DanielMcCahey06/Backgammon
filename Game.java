1fdac40326d431d9a79e29eb8a6073f102a0f5d0
Pat-rick-Hi-ck <patrick.hickey1@ucdconnect.ie>
2024-10-29 16:42:37 +0000

    Add files via upload

--- Game.java
+++ Game.java
@@ -2,9 +2,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import static java.awt.Color.WHITE;
+
 public class Game {
     private Dice dice;
+    private Player player1;
+    private Player player2;
     private boolean running = true;
+    private boolean isPlayer1Turn = true;
 
     public Game() {
         initialiseGame();
@@ -52,31 +57,40 @@ public class Game {
 
     public void start() {
         Scanner scanner = new Scanner(System.in); // allows reading from the console
-        System.out.println("Enter name 1:");
-        String player1 = scanner.nextLine();
-        System.out.println( player1 + " plays with the White Checkers");
-        System.out.println("Enter name 2:");
-        String player2 = scanner.nextLine();
-        System.out.println( player2 + " plays with the Black Checkers");
+        System.out.println("Enter name 1 (White Checker):");
+        String player1Name = scanner.nextLine();
+        player1 = new Player(player1Name, Checker.Colour.WHITE);
+
+        System.out.println("Enter name 2 (Black Checker):");
+        String player2Name = scanner.nextLine();
+        player2 = new Player(player2Name, Checker.Colour.BLACK);
 
         while (running) {
             Board.display();
-            System.out.println("Enter move:");
+            Player currentPlayer = isPlayer1Turn ? player1 : player2;
+            System.out.println(currentPlayer.getName() + "'s turn ('" + currentPlayer.getDisplay() + "' Checkers)");
+            System.out.println("Enter 'Roll' to roll the dices, or 'Q' to quit the game:");
             String action = scanner.nextLine();// stores user input into 'action'
-            processAction(action);
+            if (processAction(action)) {
+                isPlayer1Turn = !isPlayer1Turn;
+            }
         }
         scanner.close();
     }
 
-    private void processAction(String action) {
+    private boolean processAction(String action) {
         action = action.toUpperCase();
         if (action.equals("Q")) { // quit game
             running = false;
             System.out.println("Game over!");
+            return false;
         } else if (action.equals("ROLL")) {
             int[] roll = Dice.roll();
             System.out.println("Dice roll:" + Arrays.toString(roll));
+            return true;
+        } else {
+            System.out.println("Invalid action");
+            return false;
         }
-
     }
 }
